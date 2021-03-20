ae340ee77b52454f715bb7bc1de2d89cc53a0f82
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-03-20 19:17:09 -0300

    implement roles for access endpoint

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const User = require("../../app/Models/User")
-
 /*
 |--------------------------------------------------------------------------
 | DatabaseSeeder
@@ -12,8 +10,9 @@ const User = require("../../app/Models/User")
 |
 */
 
-
-const user = use('App/Models/User')
+const User = use('App/Models/User')
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
 
 class DatabaseSeeder {
 
@@ -26,11 +25,43 @@ class DatabaseSeeder {
       password: '123456'
     })
 
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar membros'
+    });
+
+    const createProject = await Permission.create({
+      slug: 'project_create',
+      name: 'Criar projetos'
+    });
+ 
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrador'
+    });
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderador'
+    });
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitante'
+    });
+
+    await admin.permissions().attach([createInvite.id, createProject.id]);
+    await moderator.permissions().attach([createProject.id]);
+
     //create a team for user
-    await user.teams().create({
+    const team = await user.teams().create({
       name: 'DevTeam',
       user_id: user.id
-    })
+    });
+
+    const teamJoin = await user.teamJoins().where('team_id',team.id).first();
+
+    await teamJoin.roles().attach([admin.id]);
   }
 }
 
